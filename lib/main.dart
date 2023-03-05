bf2222fd49d99cfa87e2bd9128a725a862229c9c
mahmud <mahmud120398@gmail.com>
2023-03-05 23:34:09 +0700

    refactor: udpate main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,12 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:my_story_app/presentation/routes/routes.dart';
 import 'package:my_story_app/utils/injectable.dart';
-import 'presentation/routes/register_routes.dart';
+import 'presentation/application.dart';
+import 'utils/loading_options.dart';
 
 void main() async {
-  await DotEnv().load();
+  await dotenv.load();
   Injectable.instance.register();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: registerRoutes,
-      initialRoute: RouteName.storyList,
-      debugShowCheckedModeBanner: kDebugMode,
-      theme: ThemeData(primarySwatch: Colors.blue),
-    );
-  }
+  configureLoadingOptions();
+  runApp(const MyStoryApplication());
 }
